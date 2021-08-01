edd78be71f2662d16b05b0af20d7b348e066024a
roeun <pacharothroeun01@gmail.com>
2021-08-01 13:46:05 +0700

    [Task] -Planning on Working Project

---
+++ public/assets/js/tictactoe.js
@@ -0,0 +1,15 @@
+// TODO We have 2 options Training option button
+
+// TODO PC vs PC (no training)
+// TODO Human vs AI 
+// TODO Training Generate (Training)
+
+class TrainingTicTacToe{
+
+}
+class HumanVsAI{
+
+}
+class PcVsPc{
+    
+}
