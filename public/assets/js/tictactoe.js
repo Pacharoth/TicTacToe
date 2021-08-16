fb24191f04a63db906c6be709093d75922b8d257
roeun <pacharothroeun01@gmail.com>
2021-08-16 20:19:04 +0700

    [Task] Start Initialize the program

--- public/assets/js/tictactoe.js
+++ public/assets/js/tictactoe.js
@@ -4,12 +4,56 @@
 // TODO Human vs AI 
 // TODO Training Generate (Training)
 
-class TrainingTicTacToe{
+//create AI training
 
+class TrainingTicTacToe{
+    constructor(){  
+
+    }
+}
+class ActionMovement{
+    constructor(){
+        this.x = 0;
+        this.O = 1;
+    }
 }
 class HumanVsAI{
+    constructor(){
 
+    }
 }
-class PcVsPc{
-    
+class UI{
+    constructor(){
+        this.button = document.createElement("button");
+        this.bigBox = document.createElement('div');
+
+    }
+    startMenuBar(){
+        //1 Button Human vs AI 2 AI vs AI 
+        let background  =   this.bigBox,
+            startgame   =   this.button,
+            root        =   this.bigBox;
+
+        background.classList.add("background_start_game");
+        root.classList.add("root");
+        document.append(root);
+        
+    }
+}
+class Main{
+    constructor(){
+
+    }
+    run(){
+        let start = new UI();
+        
+        // 0: Human vs ai  1 Training(pcvspc)
+        if(player==0){
+
+        }else if(player==1){
+
+        }else if(player==2){
+
+        }
+    }
 }
